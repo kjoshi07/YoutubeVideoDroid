71fbca5315a79c0991d628b55513c4e2ef2b4e72
Khemchand Joshi <engg.kjoshi@gmail.com>
2017-04-11 15:23:42 +0530

    initial commit

---
+++ app/src/main/java/com/kc/youtubevideodroid/MainActivity.java
@@ -0,0 +1,97 @@
+package com.kc.youtubevideodroid;
+
+import android.os.Bundle;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+import com.kc.youtubevideodroid.config.Constants;
+
+public class MainActivity extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener {
+
+    private final static String TAG = MainActivity.class.getSimpleName();
+    private YouTubePlayerView mYoutubeVideoPlayerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Initialize UI views
+        mYoutubeVideoPlayerView = (YouTubePlayerView) findViewById(R.id.youtubeVideoPlayerView);
+        mYoutubeVideoPlayerView.initialize(Constants.API_KEY, this);
+    }
+
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+        youTubePlayer.setPlayerStateChangeListener(playerStateChangeListener);
+        youTubePlayer.setPlaybackEventListener(playbackEventListener);
+
+        //start Buffering
+        youTubePlayer.cuePlaylist(Constants.YOUTUBE_VIDEO_ID);
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+    }
+
+    private YouTubePlayer.PlayerStateChangeListener playerStateChangeListener = new YouTubePlayer.PlayerStateChangeListener() {
+        @Override
+        public void onLoading() {
+
+        }
+
+        @Override
+        public void onLoaded(String s) {
+
+        }
+
+        @Override
+        public void onAdStarted() {
+
+        }
+
+        @Override
+        public void onVideoStarted() {
+
+        }
+
+        @Override
+        public void onVideoEnded() {
+
+        }
+
+        @Override
+        public void onError(YouTubePlayer.ErrorReason errorReason) {
+
+        }
+    };
+
+    private YouTubePlayer.PlaybackEventListener playbackEventListener = new YouTubePlayer.PlaybackEventListener() {
+        @Override
+        public void onPlaying() {
+
+        }
+
+        @Override
+        public void onPaused() {
+
+        }
+
+        @Override
+        public void onStopped() {
+
+        }
+
+        @Override
+        public void onBuffering(boolean b) {
+
+        }
+
+        @Override
+        public void onSeekTo(int i) {
+
+        }
+    };
+}
---
+++ app/src/main/java/com/kc/youtubevideodroid/config/Constants.java
@@ -0,0 +1,11 @@
+package com.kc.youtubevideodroid.config;
+
+/**
+ * Created by Khemchand Joshi on 11/04/17.
+ */
+
+public final class Constants {
+
+    public final static String API_KEY = "";
+    public final static String YOUTUBE_VIDEO_ID = "";
+}
